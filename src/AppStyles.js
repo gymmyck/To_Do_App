c8622f3816f2f0c824bf9525ad999b92d3daf4e6
Florin Ghimici <florindghimici@gmail.com>
2024-08-21 00:43:46 +0300

    Initial commit

---
+++ src/AppStyles.js
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  a {
+    text-decoration: none !important;
+  }
+
+  body {
+    margin: 0;
+    background:${(props) => props.theme.main};
+    overflow: hidden;
+  }
+
+  .main {
+    height: 1200px;
+    width: 1000px;
+  }
+
+  body::-webkit-scrollbar {
+    width: 12px;
+  }
+
+  body::-webkit-scrollbar-track {
+    background: green;
+    border-radius: 20px;
+  }
+
+  body::-webkit-scrollbar-thumb {
+    background-color: red;
+    border-radius: 20px;
+  }
+`;
+
+export default GlobalStyle;
---
+++ src/components/context/todoContext.tsx
@@ -0,0 +1,59 @@
+import { createContext, useState, ReactNode } from "react";
+import { useParams } from "react-router-dom";
+import { uid } from "uid";
+
+type Todo = {
+  id: string;
+  text: string;
+  isCompleted: boolean;
+};
+
+type TodoContextType = {
+  todos: Todo[];
+  addTodo: (text: string) => void;
+  completeTodo: (todo: Todo) => void;
+  removeTodo: (todo: Todo) => void;
+  getTodo: (id: string) => Todo | undefined;
+};
+
+type TodoProviderProps = {
+  children: ReactNode;
+};
+
+export const TodoContext = createContext<TodoContextType | undefined>(
+  undefined
+);
+
+export const TodoProvider = ({ children }: TodoProviderProps) => {
+  const { id } = useParams();
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodo = (text: string) => {
+    const newTodos = [...todos, { id: uid(), text: text, isCompleted: false }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (todo: Todo) => {
+    setTodos((todos) =>
+      todos.map((el) =>
+        el.id === todo.id ? { ...el, isCompleted: !el.isCompleted } : el
+      )
+    );
+  };
+
+  const removeTodo = (todo: Todo) => {
+    setTodos((todos) => todos.filter((el) => el.id !== todo.id));
+  };
+
+  const getTodo = (id: string) => {
+    return todos.find((el) => el.id === id);
+  };
+
+  return (
+    <TodoContext.Provider
+      value={{ todos, addTodo, completeTodo, removeTodo, getTodo }}
+    >
+      {children}
+    </TodoContext.Provider>
+  );
+};
